92c2e52c50cf0add297fcebc53d61ea0dba10711
gagscript-server2 <tiktokbusinessacc08@gmail.com>
2025-07-26 15:38:50 +0800

    Update Speed-Hub-X-Main_Loader.lua

--- Speed-Hub-X-Main_Loader.lua
+++ Speed-Hub-X-Main_Loader.lua
@@ -1,5 +1,5 @@
-local s1 = game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true)
-local s2 = game:HttpGet("https://api.rubis.app/v2/scrap/VlfvWkVQplQUxUYB/raw", true)
+local s1 = game:HttpGet("https://api.rubis.app/v2/scrap/VlfvWkVQplQUxUYB/raw", true)
+local s2 = game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true)
 
 -- Load s1 normally
 loadstring(s1)()
